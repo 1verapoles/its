5c1471f2023bdec69ffeca63bd07f4092020eeeb
vera <verapoles1@mail.ru>
2021-07-01 10:19:04 +0200

    feat: implement reservation

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+﻿$(document).ready(function() {
+    var calendarEl = document.getElementById('calendar');
+    var result1 = $('.result1');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      locale: 'ru',
+      dateClick: function(dat) {
+        if (!$('td[data-date=' + dat.dateStr + ']').find('.addinfo').length) {
+            alert('Эта дата уже забронирована!'); return;
+        }
+        $("#date").val(dat.dateStr);
+        $('.modal').modal('show');
+        $( ".form" ).submit(function( event ) {
+            event.preventDefault();
+            var th = $(this);
+            var st =  $("#date").val();
+            var en =  +$("#days").val();
+            var date1 = new Date(st);
+            var sum = 0;
+            for (var i = 0; i < en; i++) {
+                //console.log(i);
+                var date2 = new Date();
+               var param = new Date(date2.setDate(date1.getDate() + i)).toISOString().slice(0, 10);
+              // console.log(param);
+               var addinfo = $('td[data-date=' + param + ']').find('.addinfo');
+               //console.log(addinfo);
+               if (!addinfo.length) {alert("Эта дата уже забронирована! Ваша заявка не принята!"); return;}
+               sum += Number(addinfo.text());
+               addinfo.remove();
+            }
+            //console.log(sum);
+            result1.val(sum);
+            calendar.addEvent({
+                title: $('#name').val(),
+                start: st,
+                end: new Date(date1.setDate(date1.getDate() + en)),
+                allDay: true
+              });
+           jQuery.ajax({
+                url: th.attr("action"),
+                type: 'post',
+                data: th.serialize()          
+            }).done(function() {
+                alert("Ваша заявка принята! Стоимость бронирования " + sum);
+            setTimeout(function() {
+                th.trigger("reset");
+            }, 3000);
+            $('.modal').modal('hide');
+            });
+          });
+      }
+    });
+    calendar.render();
+    $( ".fc-daygrid-day.fc-day-mon, .fc-daygrid-day.fc-day-tue, .fc-daygrid-day.fc-day-wed, .fc-daygrid-day.fc-day-thu, .fc-daygrid-day.fc-day-fri" ).append( "<div class='addinfo'>10</div>" );
+    $( " .fc-daygrid-day.fc-day-sat, .fc-daygrid-day.fc-day-sun" ).append( "<div class='addinfo'>30</div>" );
+
+    
+
+     });   
---
+++ send.php
@@ -0,0 +1,57 @@
+﻿<?php
+// Файлы phpmailer
+
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$email = $_POST['email'];
+$date = $_POST['date'];
+$days = $_POST['days'];
+$comin = $_POST['comin'];
+
+// Формирование самого письма
+$title = "Бронирование апартментов";
+$body = "<b>$name $surname</b>, email <b>$email</b>, забронировал апартменты с $date &nbsp; $comin на $days дней.";
+
+// Настройки PHPMailer
+$mail = new phpmailer/PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'verapoles12345@gmail.com'; // Логин на почте
+    $mail->Password   = 'A123123a'; // Пароль на почте
+    $mail->SMTPSecure = 'tls';
+    $mail->Port       = 587;
+    $mail->setFrom('verapoles12345@gmail.com', 'Вера Полещук'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress(' info@itspro.by');  
+    $mail->addAddress($email); // Ещё один, если нужен
+
+   // Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+//echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+?>
